a244e939d2c1ed227bdfa3b794049ddd041069b2
Thomas Fi <tom.filhol@gmail.com>
2022-11-04 10:27:12 +0100

    Responsive

--- js/glide.js
+++ js/glide.js
@@ -1,6 +1,6 @@
 var checkbox = document.querySelector('#options-hoverpause-checkbox')
 
-const config = {
+var config = {
     type: 'carousel',
     perView: 1,
     peek: {
@@ -8,6 +8,18 @@ const config = {
         },
     autoplay: 3000,
     hoverpause: true
+}
+
+if (window.matchMedia("(max-width: 800px)").matches) {
+    config = {
+        type: 'carousel',
+        perView: 1,
+        peek: {
+            before: 0, after: 0
+            },
+        autoplay: 3000,
+        hoverpause: true
+    }
 }
 
 new Glide('.glide', config).mount()
